64c14e1d543c24e5197b0e84a57b469c49462862
Dhanush <113832882+Dhanuxh@users.noreply.github.com>
2023-08-03 16:59:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Gallary from './components/Gallary';
+
+import GallaryFooter from './components/GallaryFooter';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Gallary/>
+      <GallaryFooter/> 
+    </div>
+  )
+}
+
+export default App;
